7adec00bb441262535b9016f6e1f1cb1c589d5d6
siddharthjain919 <89840296+siddharthjain919@users.noreply.github.com>
2021-08-31 16:33:04 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,19 +6,20 @@ def view_command():
     for row in backend.show():
         lb1.insert(END, row)
 
+        
 def search_command():
-
     lb1.delete(0, END)
     for row in backend.search(title_val.get(), author_val.get(), year_val.get(), isbn_val.get()):
         lb1.insert(END, row)
 
+        
 def add_command():
     backend.insert(title_val.get(), author_val.get(), year_val.get(), isbn_val.get())
     lb1.delete(0, END)
     lb1.insert(END,(title_val.get(), author_val.get(), year_val.get(), isbn_val.get()))
 
-def get_selected_row(event):
     
+def get_selected_row(event):
     try:
         global selected_tuple
         index=lb1.curselection()[0]
@@ -33,32 +34,24 @@ def get_selected_row(event):
         e4.insert(END,selected_tuple[4])
     except IndexError:
         pass
-
-
-
+    
 def del_command():
     backend.delete(selected_tuple[0])
 
+    
 def update_command():
     backend.update(selected_tuple[0], title_val.get(), author_val.get(), year_val.get(), isbn_val.get())
 
-
     
-
-
 window= Tk()
 window.wm_title("BookStore")
 
-
 l1= Label(window, text="Title")
 l1.grid(row=0, column=0)
-
 l2= Label(window, text="Author")
 l2.grid(row=0, column=2)
-
 l3= Label(window, text="Year")
 l3.grid(row=1, column=0)
-
 l4= Label(window, text="ISBN")
 l4.grid(row=1, column=2)
 
@@ -87,24 +80,18 @@ sb1.grid(row=2, column=2, rowspan=6)
 
 lb1.configure(yscrollcommand=sb1.set)
 sb1.configure(command=lb1.yview)
-
 lb1.bind('<<ListboxSelect>>', get_selected_row)
 
 b1=Button(window, text="Show all", width=12, command=view_command)
 b1.grid(row=2, column=3)
-
 b2=Button(window, text="Search Entry", width=12, command=search_command)
 b2.grid(row=3, column=3)
-
 b3=Button(window, text="Add Entry", width=12, command=add_command)
 b3.grid(row=4, column=3)
-
 b4=Button(window, text="Update", width=12, command=update_command)
 b4.grid(row=5, column=3)
-
 b5=Button(window, text="Delete", width=12, command=del_command)
 b5.grid(row=6, column=3)
-
 b6=Button(window, text="Exit", width=12, command=window.destroy)
 b6.grid(row=7, column=3)
 
